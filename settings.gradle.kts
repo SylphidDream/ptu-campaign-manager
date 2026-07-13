cf9884075b91cd9c89109efe9a3375faa8c0b97c
SylphidDream <caabel98@gmail.com>
2026-07-13 11:32:19 -0400

    Initial project setup - Generate Spring Boot project - Configure Maven - Add initial dependencies

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "ptu-campaign-manager"
---
+++ src/main/java/com/sylphiddream/ptucampaignmanager/PtuCampaignManagerApplication.java
@@ -0,0 +1,13 @@
+package com.sylphiddream.ptucampaignmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtuCampaignManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtuCampaignManagerApplication.class, args);
+    }
+
+}
